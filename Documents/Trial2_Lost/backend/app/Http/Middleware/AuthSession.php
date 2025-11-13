1d20f00121286e5a6a8b47e77826de49495173df
ker-mari <magmayoker@gmail.com>
2025-11-13 20:30:44 +0800

    Timer Disabled Again

--- Documents/Trial2_Lost/backend/app/Http/Middleware/AuthSession.php
+++ Documents/Trial2_Lost/backend/app/Http/Middleware/AuthSession.php
@@ -13,16 +13,6 @@ class AuthSession
             return redirect()->route('pin');
         }
         
-        // // Check session timeout (5 minutes)
-        // $lastActivity = session('last_activity', 0);
-        // if (time() - $lastActivity > 300) {
-        //     session()->flush();
-        //     return redirect()->route('home');
-        // }
-        
-        // Update last activity
-        session(['last_activity' => time()]);
-        
         return $next($request);
     }
 }
