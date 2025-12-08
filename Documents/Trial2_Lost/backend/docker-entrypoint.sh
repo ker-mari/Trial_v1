fb4a005fd832101b789cd05125875426992a3f74
ker-mari <magmayoker@gmail.com>
2025-12-08 19:28:23 +0800

    Docketentrypoint fixed

--- Documents/Trial2_Lost/backend/docker-entrypoint.sh
+++ Documents/Trial2_Lost/backend/docker-entrypoint.sh
@@ -5,13 +5,19 @@ set -e
 # Laravel container startup script
 # ------------------------------
 
+# Ensure SQLite database file exists
+if [ ! -f database/database.sqlite ]; then
+    touch database/database.sqlite
+    chmod 777 database/database.sqlite
+fi
+
 
 # Run Laravel commands
 echo "Running storage link..."
 php artisan storage:link || true   # skip if already exists
 
 echo "Running migrations and seeding..."
-php artisan migrate --seed
+php artisan migrate
 
 
 
