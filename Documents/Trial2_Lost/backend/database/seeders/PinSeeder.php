17f88aa89167cb964b410d12e7466304235c021a
ker-mari <magmayoker@gmail.com>
2025-12-08 17:14:11 +0800

    DB Seeder Updated.

--- Documents/Trial2_Lost/backend/database/seeders/PinSeeder.php
+++ Documents/Trial2_Lost/backend/database/seeders/PinSeeder.php
@@ -11,21 +11,21 @@ class PinSeeder extends Seeder
     public function run(): void
     {
         Pin::create([
-            'pin_hash' => Hash::make('1234'),
+            'pin_hash' => Hash::make('391847'),
             'user_name' => 'Mrs. Laura Sabillon',
             'is_admin' => true,
             'is_active' => true
         ]);
 
         Pin::create([
-            'pin_hash' => Hash::make('5678'),
+            'pin_hash' => Hash::make('562018'),
             'user_name' => 'Bro. Ed',
             'is_admin' => false,
             'is_active' => true
         ]);
 
         Pin::create([
-            'pin_hash' => Hash::make('9012'),
+            'pin_hash' => Hash::make('123456'),
             'user_name' => 'Bro. Ramon',
             'is_admin' => false,
             'is_active' => true
